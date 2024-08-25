3da9a28e9cbd49a01665c65f21d90ea0f514a7a8
Intit-Paw-01 <geekoIntit90@gmail.com>
2024-08-26 00:04:03 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const currentTime = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("clock").textContent = currentTime;
+} 
+
+updateTime();
+setInterval(updateTime, 1000);
